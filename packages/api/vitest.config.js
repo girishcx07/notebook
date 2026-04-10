31a1b9d09e0ff7fbd7915e0a9d45488eb66056b8
girish.chaudhari <girishvishnuc98@gmail.com>
2026-04-10 19:22:47 +0530

    style: format api vitest config

--- packages/api/vitest.config.js
+++ packages/api/vitest.config.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var config_1 = require("vitest/config");
 exports.default = (0, config_1.defineConfig)({
-    test: {
-        environment: "node",
-        include: ["src/**/*.test.ts"],
-    },
+  test: {
+    environment: "node",
+    include: ["src/**/*.test.ts"],
+  },
 });
